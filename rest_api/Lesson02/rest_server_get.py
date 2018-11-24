446f14705481e55ca62ecb6c59ae945abe94aea8
Peter Nemec <peteneme@centrum.sk>
2018-11-24 08:33:40 +0100

    rest get client with flask

---
+++ rest_api/Lesson02/rest_server_get.py
@@ -0,0 +1,28 @@
+﻿from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+languages=[{'name':'python'},{'name':'C++'},{'name':'java'}]
+
+@app.route('/',methods=['GET'])
+def test():
+  return jsonify({'message' : 'It works...'})
+
+@app.route('/lang',methods=['GET'])
+def returnLangs():
+  return jsonify({'languages' : languages})
+
+@app.route('/lang/<string:name>',methods=['GET'])
+def returnLang(name):
+  langs = [language for language in languages if language['name'] == name]
+  return jsonify({'language' : langs[0]})
+
+if __name__ == '__main__':
+  app.run(debug=True, port=80)
+
+
+
+
+
+
+
+
