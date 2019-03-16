ac0ad8e7709b238b168c7ee989ec9284ab611962
Peter NEMEC <peter.nemec@orange.com>
2019-03-16 08:45:50 +0100

    bugfixing

--- nso-mix/xml_xpaths/xml_xpaths.py
+++ nso-mix/xml_xpaths/xml_xpaths.py
@@ -28,10 +28,10 @@ else:
   if len(sys.argv)>=3: xml_key=args[1]
 
 
-### GET_xml_ELEMENT ===========================================================
-def get_xml_xpaths(xml_data):
+### GET_XPATH_FROM_XMLSTRING ===================================================
+def get_xpath_from_xmlstring(xml_data):
   """
-  FUNCTION: get_xml_xpaths()
+  FUNCTION: get_xpath_from_xmlstring()
   parameters: xml_data   - xml data structure
   returns:    xpath_list - lists of all xpaths found in xml_data
   """
@@ -42,15 +42,16 @@ def get_xml_xpaths(xml_data):
     xml_data=tuple_data[1]
     if type(xml_data)==dict or type(xml_data)==collections.OrderedDict:
       for key in xml_data.keys():
-        #if not (str(key)[0]=='@' or str(key)[0]=='#'):   ###ARGUMENTS
-          key_content=xml_data.get(key)
-          if type(key_content) in [dict,collections.OrderedDict]: xml_deeper_references.append((parrent_xpath+'/'+key,key_content))
-          elif type(key_content)==list:
-            for ii,sub_xml in enumerate(key_content,start=0):
-              if type(sub_xml) in [dict,collections.OrderedDict]: xml_deeper_references.append((parrent_xpath+'/'+key+'['+str(ii)+']',sub_xml))
-          elif isinstance(key_content,str):
-            if '#text' in key: xml_deeper_references.append((parrent_xpath+'="'+key_content+'"',key_content))
-            else: xml_deeper_references.append((parrent_xpath+'/'+key+'="'+key_content+'"',key_content))
+        key_content=xml_data.get(key)
+        if type(key_content) in [dict,collections.OrderedDict]: xml_deeper_references.append((parrent_xpath+'/'+key,key_content))
+        elif type(key_content)==list:
+          for ii,sub_xml in enumerate(key_content,start=0):
+            if type(sub_xml) in [dict,collections.OrderedDict]: xml_deeper_references.append((parrent_xpath+'/'+key+'['+str(ii)+']',sub_xml))
+        elif isinstance(key_content,str):
+          if '#text' in key: xml_deeper_references.append((parrent_xpath+'="'+key_content+'"',key_content))
+          elif str(key)[0]=='@': xml_deeper_references.append((parrent_xpath+'['+key+'="'+key_content+'"]',key_content))
+          else: xml_deeper_references.append((parrent_xpath+'/'+key+'="'+key_content+'"',key_content))
+        elif key_content==None: xml_deeper_references.append((parrent_xpath+'/'+key,None))
     return xml_deeper_references
   ### FUNCTION -----------------------------------------------------------------
   references=[]
@@ -63,14 +64,44 @@ def get_xml_xpaths(xml_data):
     references=references+add_references
   del references
   return xpath_list
-  ### END OF GET_xml_ELEMENT ==================================================
+### ----------------------------------------------------------------------------
+
+
+### GET_XMLSTRING_FROM_XPATH ===================================================
+def get_xmlstring_from_xpath(xpathexpression):
+  ### trick1 = [cc=value] --> /cc=value , trick2 argument parsing= '[@' --> ' @@@@' and </ .split(' @@@@')[0]> ,trick3 @@@@ to ignore 1..2x@ in http/email
+  xml_string=str()
+  if xpathexpression:
+    if aargs.verbose: print('XPATH_ORIGINAL:',xpathexpression)
+    xpath_list=str(xpathexpression).split('/')[1:] if '/' in str(xpathexpression) else [str(xpathexpression)]
+    if aargs.verbose: print('XPATH_TAG_LIST:',xpath_list)
+    last_xml_element='<%s>\n<%s>%s</%s>\n<%s>'%(xpath_list[-1].split('[')[0],xpath_list[-1].split('[')[1].split('=')[0],xpath_list[-1].split('=')[1].replace('"','').replace("'",'').replace(']',''),xpath_list[-1].split('[')[1].split('=')[0],xpath_list[-1].split('[')[0]) if '=' in xpath_list[-1] else '<%s/>'%(xpath_list[-1])
+    xml_string=last_xml_element
+    if aargs.verbose: print('LAST_ELEMENT:',last_xml_element)
+    for element in xpath_list[::-1][1:]:
+      if '[@' in element:
+        xpath_tag=element.split('[@')[0]
+        xpath_argument=' '+element.split('[@')[1].split(']')[0] if '[@' in element else str()
+        xml_string='<'+xpath_tag+xpath_argument.replace("'",'"')+'>\n'+xml_string+'\n</'+xpath_tag+'>'
+      elif '=' in element:   pass
+        #xml_string='<%s>%s</%s>'%(element.split('[')[1].split('=')[0],element.split('=')[1].replace('"','').replace("'",''),element.split('[')[1].split('=')[0])
+        #xml_string='<'+element.replace("'",'"')+'>\n'+xml_string+'\n</'+element+'>'
+      elif '[' in element:
+        position=int(element.split('[')[1].split(']')[0])
+        xml_string='<'+element.split('[')[0]+'>\n'+xml_string+'\n</'+element.split('[')[0]+'>'
+        if position>0:
+          for i in range(position): xml_string='<'+element.split('[')[0]+'>\n'+'</'+element.split('[')[0]+'>\n'+xml_string
+  return str(xml_string)
+### ----------------------------------------------------------------------------
+
+
 
 
 ### MAIN =======================================================================
 def main():
   with io.open(fileName) as xml_file: aaa=xml_file.read();xml_raw_data = xmltodict.parse(aaa) #,process_namespaces=True)  #item_depth=2, item_callback=handle_artist
   if xml_raw_data:
-    xml_xpaths=get_xml_xpaths(xml_raw_data)
+    xml_xpaths=get_xpath_from_xmlstring(xml_raw_data)
     print('\n'.join(xml_xpaths))
     if xml_xpaths:
       now = datetime.datetime.now()
