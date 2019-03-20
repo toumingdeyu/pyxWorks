6d3215934c6863d5243891c9f9a9e0072e11bcee
Peter Nemec <peteneme@centrum.sk>
2019-03-20 03:01:57 +0100

    11 bugfix indentation

--- nso-mix/netconf_ssh_examples/netconf_client_11.py
+++ nso-mix/netconf_ssh_examples/netconf_client_11.py
@@ -164,7 +164,7 @@ def get_xpath_from_xmlstring(xml_data):
     add_references=get_xml_dictionary_subreferences(references[0])
     xpath_list.append(references[0][0])
     references.remove(references[0])
-     references=add_references+references
+    references=add_references+references
   del references
   return xpath_list
 ### ----------------------------------------------------------------------------
