9fb5f0d6f8b188e45dd80d1798b33cf7135f74aa
Peter NEMEC <peter.nemec@orange.com>
2019-03-22 17:17:12 +0100

    more colours 12

--- nso-mix/netconf_ssh_examples/netconf_client_12.py
+++ nso-mix/netconf_ssh_examples/netconf_client_12.py
@@ -430,6 +430,16 @@ def get_string_file_difference_string(old_isis_list,new_isis_list):
     ENDC='\033[0m'
     BOLD='\033[1m'
     UNDERLINE='\033[4m'
+    Red = '\033[91m'
+    Green = '\033[92m'
+    Blue = '\033[94m'
+    Cyan = '\033[96m'
+    White = '\033[97m'
+    Yellow = '\033[93m'
+    Magenta = '\033[95m'
+    Grey = '\033[90m'
+    Black = '\033[90m'
+    Default = '\033[99m'
 
   print_string="ISIS ADJACENCY STATE: ('-' missing, '+' added, '!' different)\n"
   if old_isis_list and new_isis_list:
