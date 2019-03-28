5a6a2ad0b32ed379367156756917a4e83b2c9682
Peter NEMEC <peter.nemec@orange.com>
2019-03-28 13:14:15 +0100

    added ndiff2 method

--- custom_file_compare/file_compare.py
+++ custom_file_compare/file_compare.py
@@ -23,7 +23,7 @@ class bcolors:
 parser = argparse.ArgumentParser()
 parser.add_argument("-f1", "--file1", action = "store", default = '',help = "file1")
 parser.add_argument("-f2", "--file2", action = "store", default = '',help = "file2")
-parser.add_argument("--diff", action = "store", dest = "diff", choices = ['ndiff','new'],
+parser.add_argument("--diff", action = "store", dest = "diff", choices = ['ndiff','ndiff2','new'],
                     default = 'new', help = "filediff method ndiff/new")
 parser.add_argument("-pe", "--printequals",action = "store_true", default = False,
                     help = "print equal lines")
@@ -67,8 +67,8 @@ def get_difference_string_from_string_or_list(
     RED for something going DOWN or something missing or failed.
     ORANGE for something going UP or something NEW (not present in pre-check)
     '''
-
-    print_string = note_string if note else str()
+    print_string = str()
+    if diff_method == 'new': print_string = note_string if note else str()
 
     # make list from string if is not list already
     old_lines_unfiltered = old_string_or_list if type(old_string_or_list) == list else old_string_or_list.splitlines()
@@ -99,7 +99,6 @@ def get_difference_string_from_string_or_list(
 
     # NDIFF COMPARISON METHOD
     if diff_method == 'ndiff':
-        print_string = str()
         diff = difflib.ndiff(old_lines, new_lines)
         for line in list(diff):
             try:    first_chars = line.strip()[0]+line.strip()[1]
@@ -111,6 +110,7 @@ def get_difference_string_from_string_or_list(
             elif print_equals: print_string += line + '\n'
         return print_string
 
+    # NEW COMPARISON METHOD CONTINUE
     enum_old_lines = enumerate(old_lines)
     enum_new_lines = enumerate(new_lines)
 
@@ -169,6 +169,7 @@ def get_difference_string_from_string_or_list(
             # changed line
             elif first_line_word == first_old_line_word and not new_first_words[i] in added_lines:
                 go, diff_sign, color, print_line = 'changed_line', '!', bcolors.YELLOW, line
+                print_old_line = old_line
 
                 for item in problem_list:
                     if (re.search(item,line)) != None: color = bcolors.RED
@@ -191,7 +192,7 @@ def get_difference_string_from_string_or_list(
 
             # lost line
             elif not first_line_word in lost_lines and old_line.strip():
-                go, diff_sign, color, print_line = 'lost_line', '-',  bcolors.YELLOW, old_line
+                go, diff_sign, color, print_line = 'lost_line', '-',  bcolors.RED, old_line
 
                 try:    j, old_line = next(enum_old_lines)
                 except: j, old_line = -1, str()
@@ -207,15 +208,23 @@ def get_difference_string_from_string_or_list(
                     except: i, line = -1, str()
                 # lost line on the end
                 elif not first_line_word and first_old_line_word:
-                    go, diff_sign, color, print_line = 'lost_line_on_end', '-',  bcolors.YELLOW, old_line
+                    go, diff_sign, color, print_line = 'lost_line_on_end', '-',  bcolors.RED, old_line
 
                     try:    j, old_line = next(enum_old_lines)
                     except: j, old_line = -1, str()
                 else: print('!!! PARSING PROBLEM: ',j,old_line,' -- vs -- ',i,line,' !!!')
 
             if debug: print('####### %s  %s  %s  %s\n'%(go,color,diff_sign,print_line))
-            if print_line: print_string=print_string+'%s  %s  %s%s\n'%(color,diff_sign,print_line.rstrip(),bcolors.ENDC)
-
+            if print_line:
+                if not print_old_line:
+                    print_string=print_string+'%s  %s  %s%s\n'%(color,diff_sign,print_line.rstrip(),bcolors.ENDC)
+                else:
+                    if diff_method == 'ndiff2':
+                        print_string=print_string+'%s  %s  %s%s\n'%(bcolors.RED,'-',print_old_line.rstrip(),bcolors.ENDC)
+                        print_string=print_string+'%s  %s  %s%s\n'%(bcolors.GREEN,'+',print_line.rstrip(),bcolors.ENDC)
+                    else:
+                        print_string=print_string+'%s  %s  %s%s\n'%(color,diff_sign,print_line.rstrip(),bcolors.ENDC)
+                    print_old_line=None
     return print_string
 
 
