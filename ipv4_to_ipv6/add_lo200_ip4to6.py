b3a9ef793fa0345c8066224f4330b192a40238cc
Peter NEMEC <peter.nemec@orange.com>
2019-04-15 13:22:49 +0200

    py2to3 str compatibility

--- ipv4_to_ipv6/add_lo200_ip4to6.py
+++ ipv4_to_ipv6/add_lo200_ip4to6.py
@@ -125,7 +125,7 @@ def ssh_detect_prompt(chan, debug = False):
     while not (last_line and last_but_one_line and last_line == last_but_one_line):
         if debug: print('FIND_PROMPT:',last_but_one_line,last_line)
         buff = chan.recv(9999)
-        output += buff.replace('\r','').replace('\x07','').replace('\x08','').\
+        output += buff.decode("utf-8").replace('\r','').replace('\x07','').replace('\x08','').\
                   replace('\x1b[K','').replace('\n{master}\n','')
         if '--More--' or '---(more' in buff.strip(): chan.send('\x20')
         if debug: print('BUFFER:' + buff)
@@ -149,7 +149,7 @@ def ssh_read_until_prompt_bulletproof(chan,command,prompts,debug=False):
     while not exit_loop:
         if debug: print('LAST_LINE:',prompts,last_line)
         buff = chan.recv(9999)
-        output += buff.replace('\r','').replace('\x07','').replace('\x08','').\
+        output += buff.decode("utf-8").replace('\r','').replace('\x07','').replace('\x08','').\
                   replace('\x1b[K','').replace('\n{master}\n','')
         if '--More--' or '---(more' in buff.strip(): chan.send('\x20')
         if debug: print('BUFFER:' + buff)
