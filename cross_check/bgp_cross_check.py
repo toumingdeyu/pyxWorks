33a715bd6a575a27013d459a9df92aec08a9de75
Peter NEMEC <peter.nemec@orange.com>
2019-05-14 13:45:01 +0200

    ported function update_bgpdata...

--- cross_check/bgp_cross_check.py
+++ cross_check/bgp_cross_check.py
@@ -202,37 +202,52 @@ def update_bgpdata_structure(data_address, key_name = None, value = None, \
     """
     global bgp_data
     change_applied = None
+    if debug: print("DATA_TYPE: ", type(data_address),'ID: ',id(data_address), \
+         "DATA: ", data_address)
     ### REWRITE VALUE IN DICT ON KEY_NAME POSITION
     if isinstance(data_address, (dict,collections.OrderedDict)) \
         and isinstance(key_name, (six.string_types)):
         data_address_values = data_address.keys()
-        # k,v = data_address.items()  , py3 data_address.iteritems()
         for address_key_value in data_address_values:
             if key_name and key_name == address_key_value:
                 data_address[key_name] = value
+                if debug: print('DICT[%s]=%s.'%(key_name,value))
                 change_applied = True
         else:
             if add_new_key:
                 data_address[key_name] = value
+                if debug: print('ADDED_TO_DICT[%s]=%s.'%(key_name,value))
                 change_applied = True
     ### ADD LIST POSITION if NEEDED, REWRITE VALUE IN DICT ON KEY_NAME POSITION
     elif isinstance(data_address, (list,tuple)):
-        ### ORDER_IN_LIST=[0..], LEN()=[0..]
-        if order_in_list == len(data_address):
-            data_address.append(list_append_value)
-        ### AFTER OPTIONAL ADDITION OF END OF LIST BY ONE
-        if order_in_list <= len(data_address)-1 \
-            and isinstance(data_address[order_in_list], (dict,collections.OrderedDict)):
-            data_address_values = data_address[order_in_list].keys()
-            for key_list_item in data_address_values:
-               if key_name and key_name == key_list_item:
-                   data_address[order_in_list][key_name] = value
-                   change_applied = True
-            else:
-                if add_new_key:
-                    data_address[order_in_list][key_name] = value
-                    change_applied = True
-    if debug: print(json.dumps(bgp_data, indent=2))
+        ### SIMPLY ADD VALUE TO LIST WHEN ORDER NOT INSERTED
+        if not order_in_list and not key_name:
+            if debug: print('LIST_APPENDED.')
+            data_address.append(value)
+            change_applied = True
+        else:
+            ### ORDER_IN_LIST=[0..], LEN()=[0..]
+            if order_in_list == len(data_address):
+                data_address.append(list_append_value)
+                if debug: print('LIST_APPENDED_BY_ONE_SECTION.')
+            ### AFTER OPTIONAL ADDITION OF END OF LIST BY ONE
+            if order_in_list <= len(data_address)-1 \
+                and isinstance(data_address[order_in_list], \
+                (dict,collections.OrderedDict)):
+                data_address_values = data_address[order_in_list].keys()
+                for key_list_item in data_address_values:
+                   if key_name and key_name == key_list_item:
+                       data_address[order_in_list][key_name] = value
+                       if debug: print('DICT_LIST[%s][%s]=%s.'% \
+                           (order_in_list,key_name,value))
+                       change_applied = True
+                else:
+                    if add_new_key:
+                        data_address[order_in_list][key_name] = value
+                        if debug: print('ADDED_TO_DICT_LIST[%s][%s]=%s.'% \
+                            (order_in_list,key_name,value))
+                        change_applied = True
+    if debug: print("CHANGE_APPLIED: ",change_applied)
     return change_applied
 
 
