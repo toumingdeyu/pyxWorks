f6d983ec40580f07510075187416f560794e9706
Peter NEMEC <peter.nemec@orange.com>
2019-07-29 14:02:19 +0200

    html printout

--- cgi-bin/cgi-bin_example.py
+++ cgi-bin/cgi-bin_example.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
+import sys
+import collections
 import cgi
-import cgitb
-cgitb.enable()
+import cgitb; cgitb.enable()
 
-form = cgi.FieldStorage()
-for key in form.keys():
-    variable = str(key)
-    try: value = str(form.getvalue(variable))
-    except: value = str(','.join(form.getlist(name)))
-    print('%s:%s'%(variable,value))
-
+sys.stderr = sys.stdout
+print("Content-type:text/html\r\n\r\n")
 #print("Content-Type: text/plain")
+
+
+
+def read_cgibin_post_form(debug = None):
+    # import collections
+    # import cgi
+    # import cgitb; cgitb.enable()
+    data = collections.OrderedDict()
+    form = cgi.FieldStorage()
+    for key in form.keys():
+        variable = str(key)
+        try: value = str(form.getvalue(variable))
+        except: value = str(','.join(form.getlist(name)))
+        if variable and value: data[variable] = value
+        if debug: print('%s:%s'%(variable,value))
+    return data
+
+
+if __name__ != "__main__": sys.exit(0)
+
+print "<html>"
+print "<head>"
+print "<title>READ_FORM:</title>"
+print "</head>"
+print "<body>"
+data = read_cgibin_post_form()
+for key, value in data.items(): print "<h2>%s : %s</h2>" % (key, value)
+print "</body>"
+print "</html>"
