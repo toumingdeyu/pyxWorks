aea226b3322ecb336a2239b26b4a2343c9ae8091
Peter NEMEC <peter.nemec@orange.com>
2019-08-05 13:07:27 +0200

    timeout in case of no parameters workarround to http500

--- cgi-bin/cgi-bin_class.py
+++ cgi-bin/cgi-bin_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, io, paramiko, json , copy
+import sys, os, io, paramiko, json, copy, html
 import getopt
 import getpass
 import telnetlib
@@ -19,16 +19,19 @@ import cgi
 import requests
 
 
-class CGI_handle():
+class CGI_CLI_handle():
     """
     CGI_handle - Simple class for handling CGI parameters and 
                  clean (debug) printing to HTML/CLI    
+       Notes:  - In case of cgi_parameters_error - http[500] is raised, 
+                 but at least no appache timeout occurs...
     """ 
     # import collections, cgi, six
     # import cgitb; cgitb.enable()
     def __init__(self):
         self.debug = True
         self.START_EPOCH = time.time()
+        self.cgi_parameters_error = None
         self.gci_active = None
         self.data, self.submit_form, self.username, self.password = \
             self.read_cgibin_get_post_form()
@@ -38,6 +41,12 @@ class CGI_handle():
             print("Content-type:text/html\n\n")
             print("<html><head><title>%s</title></head><body>" % 
                 (self.submit_form if self.submit_form else 'No submit'))
+        # if self.cgi_parameters_error:
+            # import cgitb; cgitb.enable()   
+            # print("Content-type:text/html\n\n")
+            # print("<html><head><title>%s</title></head><body>" % 
+                # (self.submit_form if self.submit_form else 'No submit'))         
+            # self.uprint('\nNO INSERTED CGI PARAMETERS!\n')
         
     def __del__(self):
         self.uprint('\nEND[script runtime = %d sec]. '%(time.time() - self.START_EPOCH))
@@ -45,7 +54,10 @@ class CGI_handle():
         
     def read_cgibin_get_post_form(self):
         data, submit_form, username, password = collections.OrderedDict(), '', '', ''
-        form = cgi.FieldStorage()
+        try: form = cgi.FieldStorage()
+        except: 
+            form = collections.OrderedDict()
+            self.cgi_parameters_error = True
         for key in form.keys():
             variable = str(key)
             try: value = str(form.getvalue(variable))
@@ -73,10 +85,8 @@ class CGI_handle():
 
     def __repr__(self):
         if self.gci_active:
-            print_string = 'CGI_args=%s <br/>' % \
-                ([ [k,v] for k, v in self.data.items() ])
-            #for key, value in self.data.items(): \
-            #    ("%s:%s " % (str(key), str(value)))                
+            try: print_string = 'CGI_args=' + json.dumps(self.data) + ' <br/>'
+            except: print_string = 'CGI_args=' + ' <br/>'                
         else: print_string = 'CLI_args=%s \n' % (str(sys.argv[1:]))  
         return print_string        
             
@@ -89,16 +99,16 @@ class CGI_handle():
 if __name__ != "__main__": sys.exit(0)
 
 ### CGI-BIN READ FORM ############################################
-gci_instance = CGI_handle()
+gci_instance = CGI_CLI_handle()
 print(repr(gci_instance))
-#str(gci_instance)
+#print(str(gci_instance))
 
-#uprint('LOGDIR[%s] \n'%(LOGDIR))
-CGI_handle.uprint(gci_instance,'aaa')
-gci_instance.uprint('aaa') 
-#gci_instance.uprint(aaa)          
+CGI_CLI_handle.uprint(gci_instance,'aaa')
+gci_instance.uprint('aaa')          
 gci_instance.uprint(['aaa2','aaa3'])
 gci_instance.uprint({'aaa4':'aaa5'}, tag = 'h1')
+#cgi.print_environ()
+
 
 
 
