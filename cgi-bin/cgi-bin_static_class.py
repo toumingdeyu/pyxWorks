f6f911506447831eb7a4d6d2842ef668a2ded3f1
Peter NEMEC <peter.nemec@orange.com>
2019-08-06 09:27:44 +0200

    gci print class rewritten to static

---
+++ cgi-bin/cgi-bin_static_class.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+
+import sys, os, io, paramiko, json, copy, html
+import getopt
+import getpass
+import telnetlib
+import time, datetime
+import difflib
+import subprocess
+import re
+import argparse
+import glob
+import socket
+import six
+import collections
+
+import cgi
+import cgitb; cgitb.enable()
+import requests
+
+
+class CGI_CLI(object):
+    """
+    CGI_handle - Simple statis class for handling CGI parameters and 
+                 clean (debug) printing to HTML/CLI    
+       Notes:  - In case of cgi_parameters_error - http[500] is raised, 
+                 but at least no appache timeout occurs...
+    """ 
+    # import collections, cgi, six
+    # import cgitb; cgitb.enable()
+     
+    debug = True
+    initialized = None
+    START_EPOCH = time.time()
+    cgi_parameters_error = None
+    gci_active = None
+    
+    @staticmethod        
+    def __cleanup__():
+        CGI_CLI.uprint('\nEND[script runtime = %d sec]. '%(time.time() - CGI_CLI.START_EPOCH))
+        if CGI_CLI.gci_active: print("</body></html>")
+
+    @staticmethod
+    def register_cleanup_at_exit():
+        """
+        In static class is no constructor or destructor 
+        --> Register __cleanup__ in system
+        """
+        import atexit; atexit.register(CGI_CLI.__cleanup__)
+
+    @staticmethod
+    def init_cgi():
+        CGI_CLI.START_EPOCH = time.time()
+        CGI_CLI.initialized = True 
+        CGI_CLI.data, CGI_CLI.submit_form, CGI_CLI.username, CGI_CLI.password = \
+            collections.OrderedDict(), '', '', ''   
+        try: form = cgi.FieldStorage()
+        except: 
+            form = collections.OrderedDict()
+            CGI_CLI.cgi_parameters_error = True
+        for key in form.keys():
+            variable = str(key)
+            try: value = str(form.getvalue(variable))
+            except: value = str(','.join(form.getlist(name)))
+            if variable and value and not variable in ["submit","username","password"]: 
+                CGI_CLI.data[variable] = value
+            if variable == "submit": CGI_CLI.submit_form = value
+            if variable == "username": CGI_CLI.username = value
+            if variable == "password": CGI_CLI.password = value
+        if CGI_CLI.submit_form or len(CGI_CLI.data)>0: CGI_CLI.gci_active = True
+        if CGI_CLI.gci_active:
+            import cgitb; cgitb.enable()        
+            print("Content-type:text/html\n\n")
+            print("<html><head><title>%s</title></head><body>" % 
+                (CGI_CLI.submit_form if CGI_CLI.submit_form else 'No submit'))
+        import atexit; atexit.register(CGI_CLI.__cleanup__)
+        return None
+
+    @staticmethod 
+    def uprint(text, tag = None):
+        if CGI_CLI.debug: 
+            if CGI_CLI.gci_active:
+                if tag and 'h' in tag: print('<%s>'%(tag))
+                if tag and 'p' in tag: print('<p>')
+                if isinstance(text, six.string_types): 
+                    text = str(text.replace('\n','<br/>'))
+                else: text = str(text)   
+            print(text)
+            if CGI_CLI.gci_active: 
+                print('<br/>');
+                if tag and 'p' in tag: print('</p>')
+                if tag and 'h' in tag: print('</%s>'%(tag))
+
+    @staticmethod
+    def print_args():
+        if CGI_CLI.gci_active:
+            try: print_string = 'CGI_args=' + json.dumps(CGI_CLI.data) + ' <br/>'
+            except: print_string = 'CGI_args=' + ' <br/>'                
+        else: print_string = 'CLI_args=%s \n' % (str(sys.argv[1:]))
+        CGI_CLI.uprint(print_string)
+        return print_string        
+
+
+            
+##############################################################################
+#
+# BEGIN MAIN
+#
+##############################################################################
+
+if __name__ != "__main__": sys.exit(0)
+
+### CGI-BIN READ FORM ############################################
+#CGI_CLI()
+CGI_CLI.init_cgi()
+CGI_CLI.print_args()
+
+print(repr(CGI_CLI))
+#print(str(CGI_CLI))
+
+CGI_CLI.uprint('aaa')
+CGI_CLI.uprint('aaa')          
+CGI_CLI.uprint(['aaa2','aaa3'])
+CGI_CLI.uprint({'aaa4':'aaa5'}, tag = 'h1')
+CGI_CLI.print_args()
+#cgi.print_environ()
+
+
+
+
+
+
+
+
