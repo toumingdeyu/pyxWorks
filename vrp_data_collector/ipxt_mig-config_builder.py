b47512618890c3290531152bb63b0c0e8229e7ad
Peter NEMEC <peter.nemec@orange.com>
2019-09-12 12:07:23 +0200

    bugfix write of preparation config

--- vrp_data_collector/ipxt_mig-config_builder.py
+++ vrp_data_collector/ipxt_mig-config_builder.py
@@ -1380,11 +1380,16 @@ if data:
             except: pass
             with open(logfilename,"w") as fp:
                 fp.write('DATA = ' + get_json_with_variable_name(data) + "\n\n\n")
+                fp.write('\nGW ROUTER (%s) VERIFICATION-CONFIG: \n' %(data['cgi_data'].get('ipsec-gw-router','UNKNOWN').upper())+ 50*'-'  + '\n', tag = 'h1')
+                fp.write(gw_verification_config_text)    
+                fp.write('\nPE ROUTER (%s) VERIFICATION-CONFIG: \n' % (data['cgi_data'].get('pe-router','UNKNOWN').upper())+ 50*'-'  + '\n', tag = 'h1')
+                fp.write(pe_verification_config_text)
+                
                 configtext = 'IPSEC GW ROUTER (%s) PREPARATION-CONFIG: \n' %(data['cgi_data'].get('ipsec-gw-router','UNKNOWN').upper()) + 50*'-'  + '\n'
                 configtext += gw_preparation_config_text
                 configtext += '\n\nPE ROUTER (%s) PREPARATION-CONFIG: \n' % (data['cgi_data'].get('pe-router','UNKNOWN').upper()) + 50*'-' + '\n'
                 configtext += pe_preparation_config_text 
-                configtext = 'IPSEC GW ROUTER (%s) MIGRATION-CONFIG: \n' %(data['cgi_data'].get('ipsec-gw-router','UNKNOWN').upper()) + 50*'-'  + '\n'
+                configtext += '\n\nIPSEC GW ROUTER (%s) MIGRATION-CONFIG: \n' %(data['cgi_data'].get('ipsec-gw-router','UNKNOWN').upper()) + 50*'-'  + '\n'
                 configtext += gw_migration_config_text
                 configtext += '\n\nPE ROUTER (%s) MIGRATION-CONFIG: \n' % (data['cgi_data'].get('pe-router','UNKNOWN').upper()) + 50*'-' + '\n'
                 configtext += pe_migration_config_text
