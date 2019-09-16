9f442addf13e80a8864866e5ae1d2725bdac252c
Peter Nemec <peteneme@centrum.sk>
2019-09-16 21:19:58 +0200

    more examples

--- string_py2to3_diff/string_py2to3_diff.py
+++ string_py2to3_diff/string_py2to3_diff.py
@@ -46,9 +46,33 @@ tprint(x)
 x = six.unichr(99)
 tprint(x)
 
+try:
+    x = str()
+    x = bytearray('bytearray(str,"utf-8").ascii()', 'utf-8').ascii()
+    tprint(x)
+except Exception as e: print("bytearray(str,'utf-8').ascii()" + '\tPROBLEM[' + str(e) + ']')
+
+try:
+    x = str()
+    x = u'u.ascii()'.ascii()
+    tprint(x)
+except Exception as e: print(str(x) + '\tPROBLEM[' + str(e) + ']')
+
+try:
+    x = str()
+    x = u'u.decode("utf-8")'.decode("utf-8")
+    tprint(x)
+except Exception as e: print(str(x) + '\tPROBLEM[' + str(e) + ']')
+
+try:
+    x = str()
+    x = bytearray('bytearray(str,"utf-8").decode("utf-8")', 'utf-8').decode("utf-8")
+    tprint(x)
+except Exception as e: print(str(x) + '\tPROBLEM[' + str(e) + ']')
+
 print('\n')
 
 ### TypeError: Object of type bytes is not JSON serializable
 data['bytearray(str,"utf-8")'] = bytearray('bytearray(str,"utf-8")', 'utf-8')
 try: print(json.dumps(data, indent=4))
-except Exception as e: print('PROBLEM[' + str(e) + ']')
+except Exception as e: print(str(data['bytearray(str,"utf-8")']) + '\tPROBLEM[' + str(e) + ']')
