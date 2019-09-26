f7419e0eb866535c6d017414d60f604473190340
Peter NEMEC <peter.nemec@orange.com>
2019-09-26 12:40:59 +0200

    it works also with 4sec delay

--- cgi-bin/get_status.py
+++ cgi-bin/get_status.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 
-import sys, os, io
+import sys, os, io, time
 import cgi
 import cgitb; cgitb.enable()
 
 if __name__ != "__main__": sys.exit(0)
 
 #print("Status: %s %s\r\n" % ('222',"afafff"))
-print("Content-type:text/html; charset=utf-8")
-print("Status: %s %s\r\n" % ('222',"afafff"))
+print("Content-type:text/html")
+print("Retry-After: 300")
+time.sleep(4)
+print("Status: %s %s\r\n" % ('222',""))
 #print("Retry-After: 300")
+time.sleep(0.1)
 print("\r\n\r\n")
 print("<html><head><title>%s</title></head><body>" % ('TITLE'))
 print("text body...")
