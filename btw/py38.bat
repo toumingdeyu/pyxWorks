382e0c695c1f2df9e9c9ea5848fa465b70dd7009
Peter Nemec <peteneme@centrum.sk>
2019-10-29 16:39:25 +0100

    added py38.bat

---
+++ btw/py38.bat
@@ -0,0 +1,15 @@
+@echo off
+SET PYTHONPATH=c:\python38
+
+REM Quick and easy way how to set desired python version/distribution 
+REM only for the current cmd session without influencing of other python versions
+REM WARNING/SideEffect: Do not set any prefered python paths into system variables permanently, because first-one found is used.
+REM That is reason because our path is first. Setting is temporary forcurrent session.
+REM Please locate such BAT files for more versions into Folter which is in permanent PATH.
+SET PYTHONPATHS=%PYTHONPATH%;%PYTHONPATHS%\Scripts
+REM ECHO %PATH% | FIND /I "%PYTHONPATHS%" >Nul && ( Echo Path is already set. ) || ( set PATH=%PYTHONPATH%;%PATH% )
+set PATH=%PYTHONPATH%;%PATH%
+@echo on
+echo %PATH%
+python --version
+@echo off
