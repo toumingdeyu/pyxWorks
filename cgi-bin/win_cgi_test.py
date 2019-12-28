6f8425484ca2a2895677045db744d919b1707fc7
Peter Nemec <peteneme@centrum.sk>
2019-12-28 17:23:25 +0100

    sys

--- cgi-bin/win_cgi_test.py
+++ cgi-bin/win_cgi_test.py
@@ -1,8 +1,9 @@
 ### https://serverfault.com/questions/594298/iis-wont-let-python-script-set-headers
-
-print('HTTP/1.1 Status: 200 OK')
-print('Content-type: text/html')
-print()
+import sys
+sys.stdout.write('HTTP/1.1 Status: 200 OK\nContent-type: text/html\n\n')
+#print('HTTP/1.1 Status: 200 OK')
+#print('Content-type: text/html')
+#print()
 
 print('<HTML><HEAD><TITLE>Python Sample CGI</TITLE></HEAD>')
 print('<BODY>')
