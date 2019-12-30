d0a65be95d73bca3ea235b38bc96ebcf712257ed
Peter Nemec <peteneme@centrum.sk>
2019-12-30 09:59:51 +0100

    added css style section

--- cgi-bin/win_cgi_test.py
+++ cgi-bin/win_cgi_test.py
@@ -5,8 +5,25 @@ import sys, os
 RELOAD_BUTTON = """<input type="button" value="Reload Page" onClick="document.location.reload(true)">"""
 PAGE_TITLE = 'PID' + str(os.getpid())
 
+CSS_STYLE = """
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: green;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+"""
+
+
 print('HTTP/1.1 Status: 200 OK\nContent-type: text/html; charset=utf-8\n\n')
-print('<!DOCTYPE html><HTML><HEAD><TITLE>%s</TITLE></HEAD>' % (PAGE_TITLE))
+print('<!DOCTYPE html><HTML><HEAD><TITLE>%s</TITLE><STYLE>%s</STYLE></HEAD>' % (PAGE_TITLE, CSS_STYLE))
 print('<BODY>')
 
 print('<H1>H1</H1>')
