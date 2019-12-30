ad263b8101d459d580f4458c552cdeea5a1a473a
Peter Nemec <peteneme@centrum.sk>
2019-12-30 09:30:02 +0100

    update

--- cgi-bin/win_cgi_test.py
+++ cgi-bin/win_cgi_test.py
@@ -1,18 +1,19 @@
 ### https://serverfault.com/questions/594298/iis-wont-let-python-script-set-headers
 ### https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Transfer-Encoding
-import sys
+import sys, os
 
-sys.stdout.write('HTTP/1.1 Status: 200 OK\nContent-type: text/html\n\n')
-#sys.stdout.write('HTTP/1.1 Status: 200 OK\nContent-type: text/plain\nTransfer-Encoding: chunked\n\n')
-#print('HTTP/1.1 Status: 200 OK')
-#print('Content-type: text/html')
-#print()
+RELOAD_BUTTON = """<input type="button" value="Reload Page" onClick="document.location.reload(true)">"""
+PAGE_TITLE = 'PID' + str(os.getpid())
 
-print('<HTML><HEAD><TITLE>Python Sample CGI</TITLE></HEAD>')
+print('HTTP/1.1 Status: 200 OK\nContent-type: text/html; charset=utf-8\n\n')
+print('<!DOCTYPE html><HTML><HEAD><TITLE>%s</TITLE></HEAD>' % (PAGE_TITLE))
 print('<BODY>')
-print('<H1>This is a header</H1>')
 
-print('<p>') #this is a comment
-print('this is just like most other HTML')
+print('<H1>H1</H1>')
+print('<p>') 
+print('text')
 print('<br>')
-print('</BODY>')
+
+print(RELOAD_BUTTON)
+
+print('</BODY></HTML>')
