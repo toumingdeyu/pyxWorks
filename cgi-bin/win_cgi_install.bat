a419256e1cad96e826a08a7598ee6d0bdd95b4f4
Peter Nemec <peteneme@centrum.sk>
2020-01-02 17:44:55 +0100

    added iis.msc start and instructions

--- cgi-bin/win_cgi_install.bat
+++ cgi-bin/win_cgi_install.bat
@@ -1,3 +1,6 @@
 @echo on
 echo %cd%
 dism /online /enable-feature /featurename:IIS-CGI
+c:\windows\system32\inetsrv\iis.msc
+echo open Handler Mappings, add Script Map, 
+echo Reguest path: *.py, Executable: C:\python37\python.exe %s %s, Name: some_name
