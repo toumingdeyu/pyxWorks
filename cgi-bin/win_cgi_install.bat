33d9db6004282b6aa4858294cb6f968164b74827
Peter Nemec <peteneme@centrum.sk>
2020-01-03 10:21:20 +0100

    added link

--- cgi-bin/win_cgi_install.bat
+++ cgi-bin/win_cgi_install.bat
@@ -2,5 +2,8 @@
 echo %cd%
 dism /online /enable-feature /featurename:IIS-CGI
 c:\windows\system32\inetsrv\iis.msc
+echo How-to:
+echo https://docs.microsoft.com/en-us/iis/configuration/system.webserver/fastcgi/index
 echo open Handler Mappings, add Script Map, 
 echo Reguest path: *.py, Executable: C:\python37\python.exe %s %s, Name: some_name
+rem c:\Windows\System32\inetsrv\appcmd.exe
