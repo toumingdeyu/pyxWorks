5d7535994e1c3df4ec9d9d88eeca3737aaccecd3
Peter NEMEC <peter.nemec@orange.com>
2020-02-06 13:45:13 +0100

    red warning added

--- rid_migration/rid_migration.py
+++ rid_migration/rid_migration.py
@@ -2143,6 +2143,8 @@ try:
     if CGI_CLI.cgi_active:
         CGI_CLI.uprint('<h1 style="color:blue;">%s <a href="%s" style="text-decoration: none">(v.%s)</a></h1>' % \
             (SCRIPT_NAME, changelog, CGI_CLI.VERSION()), raw = True)
+        CGI_CLI.uprint('WARNING: PLEASE BE CAREFUL WHEN USING THIS TOOL!\nSUBMITING THE FORM WILL CHANGE CONFIGURATION ON LIVE DEVICE!', \
+            tag = 'h2', color = 'red')
     else: CGI_CLI.uprint('%s (v.%s)' % (SCRIPT_NAME,CGI_CLI.VERSION()), \
               tag = 'h1', color = 'blue')
     if printall: CGI_CLI.print_args()
