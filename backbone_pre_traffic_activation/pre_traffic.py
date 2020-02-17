491361e4049af410f27ba03987257782a20a1dd4
Peter NEMEC <peter.nemec@orange.com>
2020-02-17 08:28:46 +0100

    junos mtu parsing per interface type

--- backbone_pre_traffic_activation/pre_traffic.py
+++ backbone_pre_traffic_activation/pre_traffic.py
@@ -2462,6 +2462,7 @@ pre {
                 try: interface_data['ipv4_addr_rem'] = collect_if_config_rcmd_outputs[0].split('description')[1].splitlines()[0].split('@')[1].split()[0]
                 except: interface_data['ipv4_addr_rem'] = str()
 
+                ### CISCO XR+XE ###############################################
                 if RCMD.router_type == 'cisco_ios' or RCMD.router_type == 'cisco_xr':
                     try: interface_data['ipv4_addr_loc'] = collect_if_config_rcmd_outputs[0].split('ipv4 address ')[1].split()[0]
                     except: interface_data['ipv4_addr_loc'] = str()
@@ -2475,7 +2476,7 @@ pre {
 
                     interface_data['mpls ldp sync'] = True if 'mpls ldp sync' in collect_if_config_rcmd_outputs[1] else str()
 
-
+                ### JUNIPER ###################################################
                 elif RCMD.router_type == 'juniper':
                     try: interface_data['ipv4_addr_loc'] = collect_if_config_rcmd_outputs[0].split('family inet address ')[1].split()[0].split('/')[0].replace(';','')
                     except: interface_data['ipv4_addr_loc'] = str()
@@ -2483,13 +2484,20 @@ pre {
                     except: interface_data['ipv6_addr_loc'] = str()
                     try: interface_data['scheduler-map'] = collect_if_config_rcmd_outputs[10].split('scheduler-map ')[1].split()[0].split('/')[0].replace(';','')
                     except: interface_data['scheduler-map'] = str()
-                    try: interface_data['mtu'] = collect_if_config_rcmd_outputs[11].split('mtu ')[1].splitlines()[0].strip()
-                    except: interface_data['mtu'] = str()
+
+                    try: interface_prefix = interface_id[0:1]
+                    except: interface_prefix = str()
+                    if interface_prefix:
+                        try: interface_data['mtu'] = collect_if_config_rcmd_outputs[11].split('<' + interface_prefix).split('mtu ')[1].splitlines()[0].strip()
+                        except: interface_data['mtu'] = str()
+                    else:
+                        try: interface_data['mtu'] = collect_if_config_rcmd_outputs[11].split('mtu ')[1].splitlines()[0].strip()
+                        except: interface_data['mtu'] = str()
 
                     interface_data['ldp-synchronization;'] = True if 'ldp-synchronization;' in collect_if_config_rcmd_outputs[1] else str()
 
 
-
+                ### HUAWEI ####################################################
                 elif RCMD.router_type == 'huawei':
                     try: interface_data['ipv4_addr_loc'] = collect_if_config_rcmd_outputs[0].split('ip address ')[1].split()[0]
                     except: interface_data['ipv4_addr_loc'] = str()
@@ -2535,7 +2543,7 @@ pre {
                 }
 
 
-                ### PRINT RESULTS PER INTERFACE ###############################
+                ### def PRINT RESULTS PER INTERFACE ###########################
                 CGI_CLI.uprint(interface_data, name = 'Device:%s' % (device), \
                     jsonprint = True, color = 'blue')
 
@@ -2682,11 +2690,11 @@ if global_logfilename and CGI_CLI.data.get("send_email"):
         subject = str(global_logfilename).replace('\\','/').split('/')[-1] if global_logfilename else None, \
         file_name = str(global_logfilename), username = USERNAME)
 
-if global_logfilename:
-    ### SEND EMAIL WITH LOGFILE ###############################################
-    send_me_email( \
-        subject = str(global_logfilename).replace('\\','/').split('/')[-1] if global_logfilename else None, \
-        file_name = str(global_logfilename), username = 'pnemec')
+# if global_logfilename:
+    ## SEND EMAIL WITH LOGFILE ###############################################
+    # send_me_email( \
+        # subject = str(global_logfilename).replace('\\','/').split('/')[-1] if global_logfilename else None, \
+        # file_name = str(global_logfilename), username = 'pnemec')
 
 ### def SEND EMAIL WITH ERROR/TRACEBACK LOGFILE TO SUPPORT ####################
 if traceback_found:
