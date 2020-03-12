b42d219dc38938d3bea38e013c64f8347c8b4b3a
Peter NEMEC <peter.nemec@orange.com>
2020-03-12 11:06:06 +0100

    TESTING or OLD get out from backup interfaces

--- backbone_pre_traffic_activation/pre_traffic.py
+++ backbone_pre_traffic_activation/pre_traffic.py
@@ -3206,7 +3206,8 @@ authentication {
                                     if '%s FROM %s' % (interface_data.get('name_of_remote_device',str()).upper(), device.upper()) in line.upper():
                                         local_backup_interface = str(line.split()[0]).replace('GE','Gi')
                                         if '(' in local_backup_interface: local_backup_interface = local_backup_interface.split('(')[0]
-                                        backup_if_list.append(copy.deepcopy(local_backup_interface))
+                                        if ' TESTING ' or ' OLD ' in line: pass
+                                        else: backup_if_list.append(copy.deepcopy(local_backup_interface))
                             interface_data['parallel_interfaces'] = copy.deepcopy(backup_if_list)
                         except: interface_data['parallel_interfaces'] = []
 
@@ -3295,7 +3296,8 @@ authentication {
                                     if '%s FROM %s' % (interface_data.get('name_of_remote_device',str()).upper(), device.upper()) in line.upper():
                                         local_backup_interface = str(line.split()[0]).replace('GE','Gi')
                                         if '(' in local_backup_interface: local_backup_interface = local_backup_interface.split('(')[0]
-                                        backup_if_list.append(copy.deepcopy(local_backup_interface))
+                                        if ' TESTING ' or ' OLD ' in line: pass
+                                        else: backup_if_list.append(copy.deepcopy(local_backup_interface))
                             interface_data['parallel_interfaces'] = copy.deepcopy(backup_if_list)
                         except: interface_data['parallel_interfaces'] = []
 
@@ -3397,7 +3399,8 @@ authentication {
                                         local_backup_interface = str(line.split()[0])
                                         if not '100GE' in local_backup_interface: local_backup_interface = local_backup_interface.replace('GE','Gi')
                                         if '(' in local_backup_interface: local_backup_interface = local_backup_interface.split('(')[0]
-                                        backup_if_list.append(copy.deepcopy(local_backup_interface))
+                                        if ' TESTING ' or ' OLD ' in line: pass
+                                        else: backup_if_list.append(copy.deepcopy(local_backup_interface))
                             interface_data['parallel_interfaces'] = copy.deepcopy(backup_if_list)
                         except: interface_data['parallel_interfaces'] = []
 
