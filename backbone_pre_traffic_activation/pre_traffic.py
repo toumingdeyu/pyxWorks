cb5f55e0c61868663887b7555c2dec808f78d9ba
Peter NEMEC <peter.nemec@orange.com>
2020-04-17 08:49:57 +0200

    bgp checks

--- backbone_pre_traffic_activation/pre_traffic.py
+++ backbone_pre_traffic_activation/pre_traffic.py
@@ -4588,11 +4588,27 @@ authentication {
                         try: interface_data['No of prefixes Advertised'] = collect5_if_config_rcmd_outputs[1].split('No of prefixes Advertised:')[1].split()[0]
                         except: interface_data['No of prefixes Advertised'] = str()
 
+                    elif RCMD.router_type == 'juniper':
+                        try: interface_data['IPV4 bgp group Name'] = collect5_if_config_rcmd_outputs[0].split('Name: ')[1].split()[0]
+                        except: interface_data['IPV4 bgp group Name'] = str()
+
+                        try: interface_data['IPV4 bgp group Export'] = collect5_if_config_rcmd_outputs[0].split('Export: [ ')[1].splitlines()[0].replace(']','').strip().split()
+                        except: interface_data['IPV4 bgp group Export'] = str()
+
+                        try: interface_data['IPV4 bgp group Total peers'] = collect5_if_config_rcmd_outputs[0].split('Total peers: ')[1].split()[0]
+                        except: interface_data['IPV4 bgp group Total peers'] = str()
+
+                        try: interface_data['IPV4 bgp group Established'] = collect5_if_config_rcmd_outputs[0].split('Established: ')[1].split()[0]
+                        except: interface_data['IPV4 bgp group Established'] = str()
+
+                        interface_data['IPV4 bgp group Peers'] = []
+                        for line in collect5_if_config_rcmd_outputs[0].split('Established: ')[1].strip():
+                            if '.' in line.split()[0]:
+                                interface_data['IPV4 bgp group Peers'].append(line.split()[0])
+
                     elif RCMD.router_type == 'huawei':
                         pass
 
-                    elif RCMD.router_type == 'juniper':
-                       pass
 
 
                     ###########################################################
@@ -4641,14 +4657,26 @@ authentication {
                             try: interface_data['IPV6 No of prefixes Advertised'] = collect6_if_config_rcmd_outputs[1].split('No of prefixes Advertised:')[1].split()[0]
                             except: interface_data['IPV6 No of prefixes Advertised'] = str()
 
+                        elif RCMD.router_type == 'juniper':
+                            try: interface_data['IPV6 bgp group Name'] = collect5_if_config_rcmd_outputs[0].split('Name: ')[1].split()[0]
+                            except: interface_data['IPV6 bgp group Name'] = str()
+
+                            try: interface_data['IPV6 bgp group Export'] = collect5_if_config_rcmd_outputs[0].split('Export: [ ')[1].splitlines()[0].replace(']','').strip().split()
+                            except: interface_data['IPV6 bgp group Export'] = str()
+
+                            try: interface_data['IPV6 bgp group Total peers'] = collect5_if_config_rcmd_outputs[0].split('Total peers: ')[1].split()[0]
+                            except: interface_data['IPV6 bgp group Total peers'] = str()
+
+                            try: interface_data['IPV6 bgp group Established'] = collect5_if_config_rcmd_outputs[0].split('Established: ')[1].split()[0]
+                            except: interface_data['IPV6 bgp group Established'] = str()
+
+                            interface_data['IPV6 bgp group Peers'] = []
+                            for line in collect5_if_config_rcmd_outputs[0].split('Established: ')[1].strip():
+                                if '.' in line.split()[0]:
+                                    interface_data['IPV6 bgp group Peers'].append(line.split()[0])
+
                         elif RCMD.router_type == 'huawei':
                             pass
-
-                        elif RCMD.router_type == 'juniper':
-                           pass
-
-
-
 
 
                 ### def MTU ###################################################
