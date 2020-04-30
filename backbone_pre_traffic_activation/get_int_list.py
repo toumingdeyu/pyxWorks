2d8df4b96b27a4b3cfc7b50be9ece980a3717d5e
Peter NEMEC <peter.nemec@orange.com>
2020-04-30 09:07:24 +0200

    page reload delete

--- backbone_pre_traffic_activation/get_int_list.py
+++ backbone_pre_traffic_activation/get_int_list.py
@@ -641,7 +641,7 @@ class CGI_CLI(object):
             CGI_CLI.print_result_summary()
             if CGI_CLI.cgi_active:
                 CGI_CLI.print_chunk('<p id="scriptend"></p>', raw_log = True, printall = True)
-                CGI_CLI.print_chunk('<br/><a href = "./%s">PAGE RELOAD</a>' % (pyfile), raw_log = True, printall = True)
+                #CGI_CLI.print_chunk('<br/><a href = "./%s">PAGE RELOAD</a>' % (pyfile), raw_log = True, printall = True)
 
     @staticmethod
     def get_scriptname():
