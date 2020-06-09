1347f18d5165a47a6f10bb0d646cf930d334f935
Peter NEMEC <peter.nemec@orange.com>
2020-06-09 11:13:31 +0200

    fix ()

--- backbone_pre_traffic_activation/pre_traffic.py
+++ backbone_pre_traffic_activation/pre_traffic.py
@@ -6535,7 +6535,7 @@ authentication {
 
 
                 ### def OPTICS CHECK ##########################################
-                if CUSTOMER_MODE or BB_MODE and RCMD.router_type != 'huawei':
+                if (CUSTOMER_MODE or BB_MODE) and RCMD.router_type != 'huawei':
                     if len(interface_data['interface_data'].get('bundle_members',[])) == 0:
                         for i in range(4):
                             check_interface_data_content("['interface_statistics']['Tx_Power_Lanes_dBm']['Lane %d']" % i, higher_than = interface_data['interface_statistics'].get('Tx_Power_Lanes_dBm_Warning_low'), ignore_data_existence = True)
