1562adbb47855f6fe7c07c815eb037d4c81a04fb
Peter NEMEC <peter.nemec@orange.com>
2020-07-21 13:32:14 +0200

    inputs printed

--- json_api/json_api.py
+++ json_api/json_api.py
@@ -2492,6 +2492,7 @@ th {
 
 USERNAME, PASSWORD = CGI_CLI.init_cgi(chunked = None, css_style = None, json_api = True)
 
+CGI_CLI.JSON_RESULTS['inputs'] = str(CGI_CLI.print_args(ommit_print = True))
 CGI_CLI.JSON_RESULTS['success'] = True
 CGI_CLI.JSON_RESULTS['logfile'] = 'No logfile.'
 
