06f0ee7e17b5c47cbbb99f7370d670f0bf0e1b71
Peter NEMEC <peter.nemec@orange.com>
2020-09-07 12:42:16 +0200

    makefile paths added

--- nso-mix/create_py_package/create_py.sh
+++ nso-mix/create_py_package/create_py.sh
@@ -11,6 +11,7 @@ NCS_PACKAGES_PATH=/var/opt/ncs/packages
 MYPATH=$PATH
 
 PY_FILE_TO_EDIT=$NCS_PACKAGES_PATH/$PACKAGE_NAME/python/$PACKAGE_NAME/action.py
+MAKE_FILE_TO_EDIT=$NCS_PACKAGES_PATH/$PACKAGE_NAME/src/Makefile
 
 #COMMAND_LINE="ncs-make-package --service-skeleton python $PACKAGE_NAME --dest /var/opt/ncs/packages/$PACKAGE_NAME"
 COMMAND_LINE="ncs-make-package --service-skeleton python --component-class action.Action $PACKAGE_NAME --dest $NCS_PACKAGES_PATH/$PACKAGE_NAME"
@@ -24,10 +25,11 @@ else
      sudo -E -H -u root PATH=/opt/ncs/current/bin:$MYPATH -s $COMMAND_LINE
      sleep 3
      sudo -E -H -u root sed --in-place 's/import ncs/import ncs \nfrom ncs.dp import Action/' "${PY_FILE_TO_EDIT}"
+     sudo -E -H -u root sed --in-place 's/YANGERPATH = \$(YANGPATH:%=--path %)/YANGERPATH = \$(YANGPATH:%=--path %)\nYANGPATH += ..\/..\/oti_common\/src\/yang \\\nYANGPATH += ..\/..\/com_orange_common\/src\/yang \\/' "${MAKE_FILE_TO_EDIT}"
 fi
 
 #ls -l $NCS_PACKAGES_PATH
 ls -R $NCS_PACKAGES_PATH/$PACKAGE_NAME
 
-echo "$PY_FILE_TO_EDIT"
 head "$PY_FILE_TO_EDIT"
+cat "$MAKE_FILE_TO_EDIT"
