feb2be3378b50d0256bd07ff8e5d1371e39f9dda
Peter NEMEC <peter.nemec@orange.com>
2020-09-07 14:25:20 +0200

    yang first editing

--- nso-mix/create_py_package/create_py.sh
+++ nso-mix/create_py_package/create_py.sh
@@ -12,24 +12,29 @@ MYPATH=$PATH
 
 PY_FILE_TO_EDIT=$NCS_PACKAGES_PATH/$PACKAGE_NAME/python/$PACKAGE_NAME/action.py
 MAKE_FILE_TO_EDIT=$NCS_PACKAGES_PATH/$PACKAGE_NAME/src/Makefile
+YANG_FILE_TO_EDIT=$NCS_PACKAGES_PATH/$PACKAGE_NAME/src/yang/$PACKAGE_NAME.yang
 
-#COMMAND_LINE="ncs-make-package --service-skeleton python $PACKAGE_NAME --dest /var/opt/ncs/packages/$PACKAGE_NAME"
 COMMAND_LINE="ncs-make-package --service-skeleton python --component-class action.Action $PACKAGE_NAME --dest $NCS_PACKAGES_PATH/$PACKAGE_NAME"
 
 if [ `whoami` = root ]
 then
      $COMMAND_LINE
      sleep 3
-     #sed --in-place  's/import ncs/import ncs \nfrom ncs.dp import Action/' "${PY_FILE_TO_EDIT}"
+     sed --in-place  's/import ncs/import ncs \nfrom ncs.dp import Action/' "${PY_FILE_TO_EDIT}"
+     sed --in-place 's/YANGERPATH = \$(YANGPATH:%=--path %)/YANGERPATH = \$(YANGPATH:%=--path %)\nYANGPATH += ..\/..\/oti_common\/src\/yang \\\nYANGPATH += ..\/..\/com_orange_common\/src\/yang \\/' "${MAKE_FILE_TO_EDIT}"
+     sed --in-place 's/^  description/  import oti_common \{\n    prefix oti;\n  \}\n  import com_orange_common \{\n    prefix orange;\n  \}\n\n  description/' "${YANG_FILE_TO_EDIT}"
+
 else
      sudo -E -H -u root PATH=/opt/ncs/current/bin:$MYPATH -s $COMMAND_LINE
      sleep 3
      sudo -E -H -u root sed --in-place 's/import ncs/import ncs \nfrom ncs.dp import Action/' "${PY_FILE_TO_EDIT}"
      sudo -E -H -u root sed --in-place 's/YANGERPATH = \$(YANGPATH:%=--path %)/YANGERPATH = \$(YANGPATH:%=--path %)\nYANGPATH += ..\/..\/oti_common\/src\/yang \\\nYANGPATH += ..\/..\/com_orange_common\/src\/yang \\/' "${MAKE_FILE_TO_EDIT}"
+     sudo -E -H -u root sed --in-place 's/^  description/  import oti_common \{\n    prefix oti;\n  \}\n  import com_orange_common \{\n    prefix orange;\n  \}\n\n  description/' "${YANG_FILE_TO_EDIT}"
+
 fi
 
-#ls -l $NCS_PACKAGES_PATH
 ls -R $NCS_PACKAGES_PATH/$PACKAGE_NAME
 
 head "$PY_FILE_TO_EDIT"
 cat "$MAKE_FILE_TO_EDIT"
+cat "$YANG_FILE_TO_EDIT"
