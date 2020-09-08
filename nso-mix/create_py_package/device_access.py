7e0cb4aabf8ec93df7e10745c9a5a31976cd6ef4
Peter NEMEC <peter.nemec@orange.com>
2020-09-08 15:59:43 +0200

    junos fix

--- nso-mix/create_py_package/device_access.py
+++ nso-mix/create_py_package/device_access.py
@@ -67,7 +67,7 @@ def device_command(self, uinfo, input, device, cmd):
                     command.args = cmd_data.get("huawei-vrp")
                 else: command.args = [ cmd_data.get("huawei-vrp") ]
 
-                command_output = dev.live_status.vrp_stats__exec.any(command)
+                command_output = dev.live_status.vrp_stats__exec(command)
                 self.log.info('OUTPUT: {}'.format(command_output.result))
                 result = command_output.result
 
@@ -75,17 +75,19 @@ def device_command(self, uinfo, input, device, cmd):
             elif dev.platform.name == "junos" and cmd_data.get("junos"):
                 command_input = dev.rpc.jrpc__rpc_request_shell_execute.request_shell_execute.get_input()
 
-                #command_input.command = 'cli show version \| match Junos'
                 if isinstance(cmd_data.get("junos"), (list,tuple)):
-                    command.args = []
                     for item in cmd_data.get("junos"):
-                        command.args.append('cli ' + item.replace('|','\|'))
-                else: command.args = [ 'cli ' + cmd_data.get("junos").replace('|','\|') ]
-
-                self.log.info(command_input.command)
-                command_output = dev.rpc.jrpc__rpc_request_shell_execute.request_shell_execute.request(command_input)
-                self.log.info(command_output.output)
-                result = command_output.output
+                        command_input.command = 'cli ' + item.replace('|','\|')
+                        self.log.info(command_input.command)
+                        command_output = dev.rpc.jrpc__rpc_request_shell_execute.request_shell_execute.request(command_input)
+                        self.log.info(command_output.output)
+                        result = result + '\n' + command_output.output
+                else: 
+                    command_input.command = [ 'cli ' + cmd_data.get("junos").replace('|','\|') ]
+                    self.log.info(command_input.command)
+                    command_output = dev.rpc.jrpc__rpc_request_shell_execute.request_shell_execute.request(command_input)
+                    self.log.info(command_output.output)
+                    result = command_output.output
 
         except Exception as E:
             self.log.info("Exception device_command(): ", str(E))
