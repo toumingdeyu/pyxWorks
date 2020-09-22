53cf0a820eb8eec2fcbd3306273f4ab9c3e3934d
Peter NEMEC <peter.nemec@orange.com>
2020-09-22 15:34:17 +0200

    make package

---
+++ nso-mix/create_py_package/make_py.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NCS_PACKAGES_PATH=/var/opt/ncs/packages
+
+if [ -z $1 ]
+then
+	ls $NCS_PACKAGES_PATH
+	echo ...Please insert Python package name as 1st parameter...
+	exit 1
+fi
+
+PACKAGE_NAME=$1
+MYPATH=$PATH
+MY_USER=`whoami`
+ACTUAL_DATE=`date +%Y%m%d_%H%M`
+ACTUAL_DIR=`pwd`
+
+if [ `whoami` = root ]
+then
+
+     make -C $NCS_PACKAGES_PATH/$PACKAGE_NAME/src
+
+else
+
+     #sudo -E -H -u root tar -cvf /home/$MY_USER/$PACKAGE_NAME.$ACTUAL_DATE.tar.gz $NCS_PACKAGES_PATH/$PACKAGE_NAME
+     sudo -E -H -u root make -C $NCS_PACKAGES_PATH/$PACKAGE_NAME/src
+
+fi
+
