fbfc1a4def18ebfe3b4bf84f3b25c2ce37a84557
Peter NEMEC <peter.nemec@orange.com>
2020-09-28 14:16:03 +0200

    new script

---
+++ nso-mix/create_py_package/tailf_py.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NCS_PACKAGES_PATH=/var/opt/ncs/packages
+
+if [ -z $1 ]
+then
+	ls $NCS_PACKAGES_PATH
+	echo ...Please insert Python package name as 1st parameter...
+	exit 1
+fi
+
+PACKAGE_NAME=$1
+MYPATH=$PATH
+MY_USER=`whoami`
+ACTUAL_DATE=`date +%Y%m%d_%H%M`
+ACTUAL_DIR=`pwd`
+
+tail -f /var/log/ncs/ncs-python-vm-$PACKAGE_NAME.log
+
+
