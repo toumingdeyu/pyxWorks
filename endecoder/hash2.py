d210d4c85064c1e5afc9b8d10e280738fcdf5dbc
Peter NEMEC <peter.nemec@orange.com>
2020-11-25 11:12:25 +0100

    enc/dec, working on py2&3, unpaddind of nonprintable chars ok

--- endecoder/hash2.py
+++ endecoder/hash2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, io, time, binascii, base64, random, json
+import sys, os, io, time, binascii, base64, random, json, string
 
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
@@ -10,22 +10,38 @@ from Crypto import Random
 key_lenght = 32
 iv_lenght = 16
 
-ciphertext = str()
-### key and must be bytes
-key = str(32*' ')  ### must be 32 char length
+
+### key and iv must be bytes, key must be 32 char length
+key = "" 
 key = str.encode(key)
+
 iv = key[:16]
+
+def encrypt(key, iv, text):
+    cipher = AES.new( key, AES.MODE_CBC, iv )
+    return base64.b64encode( cipher.encrypt( text ) ) 
 
 def decrypt(key, iv, ciphertext):
     assert len(key) == key_lenght
     assert len(iv) == iv_lenght
     ciphertext = base64.b64decode(ciphertext)
     aes = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = aes.decrypt(ciphertext)        
-    return plaintext
+    plain_text = aes.decrypt(ciphertext).decode('utf-8').strip()
+    readable_text = str()
+    for c in plain_text:
+        if c in string.printable: readable_text += c    
+    return readable_text 
 
-plaintext = decrypt(key, iv, ciphertext)
-print(plaintext)
+text = ''
+text += (16-len(text)%16) * " "
+text = str.encode(text)
+
+cipher_text = encrypt(key, iv, text)
+print(cipher_text)
+
+plain_text = decrypt(key, iv, cipher_text)
+print(plain_text)
+
 
 
 sys.exit(0)
