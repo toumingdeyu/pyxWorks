5ec1d3c1d7701d41fac5c8676ec173b30ddb840c
Peter NEMEC <peter.nemec@orange.com>
2020-12-04 11:17:01 +0100

    conf diff

--- router_sw_update/sw_upgrade_pre_post_checker.py
+++ router_sw_update/sw_upgrade_pre_post_checker.py
@@ -2714,6 +2714,233 @@ def read_section_from_logfile(section = None, logfile = None):
     return text
 
 ##############################################################################
+default_problemline_list   = []
+default_ignoreline_list    = [r' MET$', r' UTC$']
+default_linefilter_list    = []
+default_compare_columns    = []
+default_printalllines_list = []
+
+
+def get_difference_string_from_string_or_list(
+    old_string_or_list, \
+    new_string_or_list, \
+    diff_method = 'ndiff0', \
+    ignore_list = default_ignoreline_list, \
+    problem_list = default_problemline_list, \
+    printalllines_list = default_printalllines_list, \
+    linefilter_list = default_linefilter_list, \
+    compare_columns = [], \
+    print_equallines = None, \
+    tolerance_percentage = None, \
+    debug = None, \
+    note = True,
+    use_twice_tolerance_for_small_values = None ):
+    '''
+    FUNCTION get_difference_string_from_string_or_list:
+    INPUT PARAMETERS:
+      - OLD_STRING_OR_LIST - content of old file in string or list type
+      - NEW_STRING_OR_LIST - content of new file in string or list type
+      - DIFF_METHOD - ndiff, ndiff0, pdiff0
+      - IGNORE_LIST - list of regular expressions or strings when line is ignored for file (string) comparison
+      - PROBLEM_LIST - list of regular expressions or strings which detects problems, even if files are equal
+      - PRINTALLLINES_LIST - list of regular expressions or strings which will be printed grey, even if files are equal
+      - LINEFILTER_LIST - list of REGEXs! which are filtered-out items for comparison (deleted each item in line)
+      - COMPARE_COLUMNS - list of columns which are intended to be different , other columns in line are ignored
+      - PRINT_EQUALLINES - True/False prints all equal new file lines with '=' prefix , by default is False
+      - TOLERANCE_PERCENTAGE - All numbers/selected columns in line with % tolerance. String columns must be equal.
+      - USE_TWICE_TOLERANCE_FOR_SMALL_VALUES - By default it is off , it slows down check performance
+      - DEBUG - True/False, prints debug info to stdout, by default is False
+      - NOTE - True/False, prints info header to stdout, by default is True
+    RETURNS: string with file differencies , all_ok [True/False]
+
+    PDIFF0 FORMAT: The head of line is
+    '-' for missing line,
+    '+' for added line,
+    '!' for line that is different and
+    ' ' for the same line, but with problem.
+    RED for something going DOWN or something missing or failed.
+    ORANGE for something going UP or something NEW (not present in pre-check)
+    '''
+    print_string, all_ok = str(), True
+    if note:
+       print_string = "DIFF_METHOD: "
+       if diff_method   == 'ndiff0': print_string += note_ndiff0_string
+       elif diff_method == 'pdiff0': print_string += note_pdiff0_string
+       elif diff_method == 'ndiff' : print_string += note_ndiff_string
+
+    # MAKE LIST FROM STRING IF IS NOT LIST ALREADY -----------------------------
+    old_lines_unfiltered = old_string_or_list if type(old_string_or_list) == list else old_string_or_list.splitlines()
+    new_lines_unfiltered = new_string_or_list if type(new_string_or_list) == list else new_string_or_list.splitlines()
+
+    # NDIFF COMPARISON METHOD---------------------------------------------------
+    if diff_method == 'ndiff':
+        diff = difflib.ndiff(old_lines_unfiltered, new_lines_unfiltered)
+        for line in list(diff):
+            try:    first_chars = line[0]+line[1]
+            except: first_chars = str()
+            ignore = False
+            for ignore_item in ignore_list:
+                if (re.search(ignore_item,line)) != None: ignore = True
+            if ignore: continue
+            if len(line.strip())==0: pass
+            elif '+ ' == first_chars: print_string += COL_ADDED + line + bcolors.ENDC + '\n'
+            elif '- ' == first_chars: print_string += COL_DELETED + line + bcolors.ENDC + '\n'
+            elif '? ' == first_chars or first_chars == str(): pass
+            elif print_equallines: print_string += COL_EQUAL + line + bcolors.ENDC + '\n'
+            else:
+                print_line, ignore = False, False
+                for item in printalllines_list:
+                    if (re.search(item,line)) != None: print_line = True
+                if print_line:
+                    print_string += COL_EQUAL + line + bcolors.ENDC + '\n'
+        if print_string: all_ok = False
+        return print_string, all_ok
+
+    # NDIFF0 COMPARISON METHOD--------------------------------------------------
+    if diff_method == 'ndiff0' or diff_method == 'pdiff0':
+        ignore_previous_line = False
+        diff = difflib.ndiff(old_lines_unfiltered, new_lines_unfiltered)
+        listdiff_nonfiltered = list(diff)
+        listdiff = []
+        # FILTER OUT - DIFF LINES OUT OF '? ' AND VOID LINES -------------------
+        for line in listdiff_nonfiltered:
+            # IGNORE_LIST FILTER - THISONE IS MUCH FASTER ----------------------
+            ignore = False
+            for ignore_item in ignore_list:
+                if (re.search(ignore_item,line)) != None: ignore = True
+            if ignore: continue
+            try:    first_chars = line[0]+line[1]
+            except: first_chars = str()
+            if '+ ' in first_chars or '- ' in first_chars or '  ' in first_chars:
+                listdiff.append(line)
+        del diff, listdiff_nonfiltered
+        # MAIN NDIFF0/PDIFF0 LOOP PER LISTDIFF LIST ----------------------------
+        previous_minus_line_is_change = False
+        for line_number,line in enumerate(listdiff):
+            print_color, print_line = COL_EQUAL, str()
+            try:    first_chars_previousline = listdiff[line_number-1][0]+listdiff[line_number-1][1].replace('\t',' ')
+            except: first_chars_previousline = str()
+            try:    first_chars = line[0]+line[1].replace('\t',' ')
+            except: first_chars = str()
+            try:    first_chars_nextline = listdiff[line_number+1][0]+listdiff[line_number+1][1].replace('\t',' ')
+            except: first_chars_nextline = str()
+            # CHECK IF ARE LINES EQUAL AFTER FILTERING -------------------------
+            split_line,split_next_line,linefiltered_line,linefiltered_next_line = str(),str(),str(),str()
+            ### CLEAR AUXILIARY VARIABLES WHEN NO -/+ FIRST CHARACTERS ---------
+            if first_chars.strip() == str():
+                ignore_previous_line = False
+                previous_minus_line_is_change = False
+            ### POSSIBLE CHANGE in LINE ----------------------------------------
+            if '-' in first_chars and '+' in first_chars_nextline:
+                ### SELECT COMPARE_COLUMNS -------------------------------------
+                for split_column in compare_columns:
+                    # +1 MEANS EQUAL OF DELETION OF FIRST COLUMN -
+                    try: temp_column = line.split()[split_column+1]
+                    except: temp_column = str()
+                    split_line += ' ' + temp_column.replace('/',' ')
+                for split_column in compare_columns:
+                    # +1 MEANS EQUAL OF DELETION OF FIRST COLUMN +
+                    try: temp_column = listdiff[line_number+1].split()[split_column+1]
+                    except: temp_column = str()
+                    split_next_line += ' ' + temp_column.replace('/',' ')
+                ### LINEFILTER_LIST --------------------------------------------
+                try: next_line = listdiff[line_number+1]
+                except: next_line = str()
+                try:
+                    linefiltered_next_line = re.sub(r'^[+-] ', '', next_line)
+                    linefiltered_line = re.sub(r'^[+-] ', '', line)
+                except:
+                    linefiltered_next_line, linefiltered_line = str(), str()
+                for linefilter_item in linefilter_list:
+                    if linefiltered_line:
+                        try: linefiltered_line = re.sub(linefilter_item, '', linefiltered_line)
+                        except: pass
+                    if linefiltered_next_line:
+                        try: linefiltered_next_line = re.sub(linefilter_item, '', linefiltered_next_line)
+                        except: pass
+                ### IF SPLIT_LINE DOES not EXIST FROM COMPARE_COLUMNS DO IT ----
+                if not split_line: split_line = line.replace('/',' ')
+                if not split_next_line: split_next_line = listdiff[line_number+1].replace('/',' ')
+                ### TOLERANCE_PERCENTAGE = COMPARING NUMBER COLUMNS with TOLERANCE
+                columns_are_equal = 0
+                for split_column,split_next_column in zip(split_line.split()[1:],split_next_line.split()[1:]):
+                    try: next_column_is_number = float(split_next_column.replace(',','').replace('%','').replace('(',''))
+                    except: next_column_is_number = None
+                    try: column_is_number = float(split_column.replace(',','').replace('%','').replace('(',''))
+                    except: column_is_number = None
+                    if column_is_number and next_column_is_number and tolerance_percentage:
+                        if not use_twice_tolerance_for_small_values or column_is_number>100:
+                            if column_is_number <= next_column_is_number * ((100 + float(tolerance_percentage))/100)\
+                                and column_is_number >= next_column_is_number * ((100 - float(tolerance_percentage))/100):
+                                    columns_are_equal += 1
+                        ### FOR SMALL VALUES UP to 100 , use TWICE TOLERANCE_PERCENTAGE - SLOW!!!
+                        else:
+                            if column_is_number <= next_column_is_number * ((100 + float(2 * tolerance_percentage))/100)\
+                                and column_is_number >= next_column_is_number * ((100 - float(2 * tolerance_percentage))/100):
+                                    columns_are_equal += 1
+                    elif split_column and split_next_column and split_column == split_next_column:
+                        columns_are_equal += 1
+                ### IF LINES ARE EQUAL WITH +/- TOLERANCE ----------------------
+                if columns_are_equal > 0 and columns_are_equal + 1 == len(split_line.split()) \
+                    and columns_are_equal + 1 == len(split_next_line.split()):
+                        ignore_previous_line = True
+                        continue
+                ### LINES ARE EQUAL AFTER FILTERING - filtered linefilter and columns commands
+                if (split_line and split_next_line and split_line == split_next_line) or \
+                   (linefiltered_line and linefiltered_next_line and linefiltered_line == linefiltered_next_line):
+                    ignore_previous_line = True
+                    continue
+            # CONTINUE CHECK DELETED/ADDED LINES--------------------------------
+            if '-' in first_chars:
+                ignore_previous_line = False
+                # FIND IF IT IS CHANGEDLINE OR DELETED LINE
+                line_list_lenght, the_same_columns = len(line.split()), 0
+                percentage_of_equality = 0
+                try: nextline_sign_column = listdiff[line_number+1].split()[0]
+                except: nextline_sign_column = str()
+                if nextline_sign_column == '+':
+                    for column_number,column in enumerate(line.split()):
+                        try: next_column = listdiff[line_number+1].split()[column_number]
+                        except: next_column = str()
+                        if column == next_column: the_same_columns += 1
+                    if line_list_lenght>0:
+                        percentage_of_equality = (100*the_same_columns)/line_list_lenght
+                # CHANGED LINE -------------------------------------------------
+                if percentage_of_equality > 54:
+                    previous_minus_line_is_change = True
+                    if diff_method == 'ndiff0':
+                        print_color, print_line = COL_DIFFDEL, line
+                # LOST/DELETED LINES -------------------------------------------
+                else: print_color, print_line = COL_DELETED, line
+            # IGNORE EQUAL -/= LINES or PRINT printall and problem lines -------
+            elif '+' in first_chars and ignore_previous_line:
+                line = ' ' + line[1:]
+                ignore_previous_line = False
+            # ADDED NEW LINE ---------------------------------------------------
+            elif '+' in first_chars and not ignore_previous_line:
+                if previous_minus_line_is_change:
+                    previous_minus_line_is_change = False
+                    if diff_method == 'pdiff0': line = '!' + line[1:]
+                    print_color, print_line = COL_DIFFADD, line
+                else: print_color, print_line = COL_ADDED, line
+            # PRINTALL ---------------------------------------------------------
+            elif print_equallines: print_color, print_line = COL_EQUAL, line
+            # PRINTALL LINES GREY IF not ALREADY PRINTED BY ANOTHER COLOR ------
+            if not print_line:
+                for item in printalllines_list:
+                    if (re.search(item,line)) != None: print_color, print_line = COL_EQUAL, line
+            # PROBLEM LIST - IN CASE OF DOWN/FAIL WRITE ALSO EQUAL VALUES !!! --
+            for item in problem_list:
+                if (re.search(item,line)) != None: print_color, print_line = COL_PROBLEM, line
+            # TEST if ALL_OK ---------------------------------------------------
+            if len(print_line)>0:
+                if print_color == COL_PROBLEM or print_line[0] in ['+','-','!']: all_ok = False
+            # Final PRINT ------------------------------------------------------
+            if print_line: print_string += "%s%s%s\n" % (print_color,print_line,bcolors.ENDC)
+    return print_string, all_ok
+
+
+
 
 
 ##############################################################################
@@ -3463,10 +3690,38 @@ try:
                     precheck_config = read_section_from_logfile('REMOTE_COMMAND: show running-config', precheck_file)
                     precheck_admin_config = read_section_from_logfile('REMOTE_COMMAND: admin show running-config', precheck_file)
 
+                    if not precheck_config:
+                        text = "precheck config is VOID!"
+                        CGI_CLI.add_result(text, 'error')
+
+                    if not precheck_admin_config:
+                        text = "precheck admin config is VOID!"
+                        CGI_CLI.add_result(text, 'error')
+
+                    if not postcheck_config:
+                        text = "postcheck config is VOID!"
+                        CGI_CLI.add_result(text, 'error')
+
+                    if not postcheck_admin_config:
+                        text = "postcheck admin config is VOID!"
+                        CGI_CLI.add_result(text, 'error')
 
 
+                    if precheck_config and postcheck_config:
+                        diff_result, all_ok = get_difference_string_from_string_or_list( \
+                            precheck_config, postcheck_config)
+                        if all_ok: pass
+                        else:
+                            text = "configs difference PROBLEM[%s]" % (diff_result)
+                            CGI_CLI.add_result(text, 'error')
 
-
+                    if precheck_admin_config and postcheck_admin_config:
+                        diff_result, all_ok = get_difference_string_from_string_or_list( \
+                            precheck_admin_config, postcheck_admin_config)
+                        if all_ok: pass
+                        else:
+                            text = "admin configs difference PROBLEM[%s]" % (diff_result)
+                            CGI_CLI.add_result(text, 'error')
 
                     # ### FIND LAST PRECHECK???? CONFIG FILE !!! ################
                     # admin_config_files, config_files = [], []
