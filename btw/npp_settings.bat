088808af649266b5905eb9642d0005fb6d305a08
Peter Nemec <peteneme@centrum.sk>
2020-12-22 11:18:12 +0100

    settings npp

---
+++ btw/npp_settings.bat
@@ -0,0 +1,9 @@
+@echo off
+cp pene.xml c:\Users\p\AppData\Roaming\Notepad++\themes\
+ECHO menu: view / function list
+ECHO settings/ style configurator/ select theme
+ECHO SMART TABS: settings / menu preferencies / lnguagge / replace by space (right down)
+
+
+
+
